1f1f2e2d85ea34cb23b3ba208a9f888aabce2a54
shrutisrivastava174 <67320113+shrutisrivastava174@users.noreply.github.com>
2020-06-27 19:04:45 +0530

    Add files via upload
    com1

---
+++ JavaScriptAssignment/JavaScript.js
@@ -0,0 +1,8 @@
+﻿function Divide(a, b) {
+    let c = parseInt(a) / parseInt(b);
+    console.log(c)
+}
+function Multiply(a, b) {
+    let c = parseInt(a) * parseInt(b);
+    console.log(c)
+}
---
+++ JavaScriptAssignment/lib.js
@@ -0,0 +1,30 @@
+﻿var today = new Date();
+var day = today.getDay();
+var daylist = ["Sunday", "Monday", "Tuesday", "Wednesday ", "Thursday", "Friday", "Saturday"];
+console.log("Today is : " + daylist[day] + ".");
+var hour = today.getHours();
+var minute = today.getMinutes();
+var second = today.getSeconds();
+var t = (hour >= 12) ? " PM " : " AM ";
+hour = (hour >= 12) ? hour - 12 : hour;
+if (hour === 0 && t === ' PM ') {
+    if (minute === 0 && second === 0) {
+        hour = 12;
+        t = ' Noon';
+    }
+    else {
+        hour = 12;
+        t = ' PM';
+    }
+}
+if (hour === 0 && t === ' AM ') {
+    if (minute === 0 && second === 0) {
+        hour = 12;
+        t = ' Midnight';
+    }
+    else {
+        hour = 12;
+        t = ' AM';
+    }
+}
+console.log("Current Time : " + hour + t + " : " + minute + " : " + second);
